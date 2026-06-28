6005e9c7cbaf2dc65870d9de2e80bb200555a2aa
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-06-28 16:10:02 +0800

    week 1 and week 2

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,15 +7,30 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut res_v = Vec::new();
+    for x in v.iter() {
+        res_v.push(*x + n);
+    }
+    res_v
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for x in v.iter_mut() {
+        *x += n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut exist_elm = HashSet::new();
+    let mut i = 0;
+    while i < v.len() {
+        if exist_elm.contains(&v[i]) {
+            v.remove(i);
+        } else {
+            exist_elm.insert(v[i]);
+            i += 1;
+        }
+    }
 }
 
 #[cfg(test)]
--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -37,4 +37,51 @@ fn main() {
     // println!("random word: {}", secret_word);
 
     // Your code here! :)
+    println!("Welcome to CS110L Hangman!");
+
+    let mut counter = NUM_INCORRECT_GUESSES;
+    let mut current_word = vec!['-'; secret_word_chars.len()];
+
+    while counter > 0 {
+        println!("The word so far is {}", current_word.iter().collect::<String>());
+        println!("You have guessed the following letters:");
+        println!("You have {} guesses left", counter);
+        println!("Please guess a letter: ");
+        io::stdout()
+            .flush()
+            .expect("Error flushing stdout.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+
+        let guess = guess.trim().chars().next().expect("Please input a letter.");
+
+        if secret_word.contains(&guess.to_string()) {
+            let mut i = 0;
+            while i < secret_word.len() {
+                if secret_word_chars[i] == guess {
+                    current_word[i] = guess;
+                }
+                i += 1;
+            }
+            if secret_word == current_word.iter().collect::<String>() {
+                break;
+            }
+        }
+        else {
+            println!("Sorry, that letter is not in the word");
+            counter -= 1;
+            if counter == 0 {
+                break;
+            }
+        }
+        println!("");
+    }
+
+    if counter > 0 {
+        println!("Congratulations you guessed the secret word: {}!", secret_word);
+    } else {
+        println!("Sorry, you ran out of guesses!");
+    }
 }
